a4941f63220d08ff169a1e5ffbf0458bcbdf44ed
yaushii <justine.telmann@gmail.com>
2018-11-26 14:34:18 +0100

    mise a jour

--- damier2.js
+++ damier2.js
@@ -17,6 +17,7 @@ function solve() {
             cell.className = "cell"
             row.appendChild(cell);
         }
+
         maindamier.appendChild(row);
     }
 }
@@ -43,38 +44,3 @@ function solve() {
 
 
 
-/*const EVEN_CLASS = 'black';
-const ODD_CLASS = 'white';
-const grid = document.querySelector('main');
-const form = document.querySelector('form');
-const rowInput = document.querySelector('#row-input');
-const colInput = document.querySelector('#col-input');
-
-function makeCell(isEven){
-  let cell = document.createElement('div');
-  cell.classList.add( isEven ? EVEN_CLASS : ODD_CLASS)
-  grid.appendChild(cell);  
-}
-
-function makeGrid(event){
-  event.preventDefault();
-  const MAX_ROWS = rowInput.value;
-  const MAX_COLUMNS = colInput.value ;
-  
-  if (!MAX_ROWS || !MAX_COLUMNS){
-    alert('Please specify a number of rows AND columns.')
-    return;
-  }
-  
-  grid.innerHTML = '';
-  grid.style.setProperty('--col-size', MAX_COLUMNS);
-  grid.style.setProperty('--row-size', MAX_ROWS);
-
-  for (let i = 0; i < MAX_ROWS; i++){
-	  for (let j = 0; j < MAX_COLUMNS; j++){
-    	makeCell((i+j) % 2 === 0);
-    }
-  }
-}
-
-form.addEventListener('submit', makeGrid);*/
